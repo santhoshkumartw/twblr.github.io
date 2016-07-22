057113e2a8eafec0f8af3c8ab9a33ce84e1869dd
Santhosh Kumar M <saam.cit@gmail.com>
2016-07-22 16:07:59 +0530

    Fixed CollatzChain

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,13 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	var i int
+	for i=0;n > 1;i++ {
+		if n%2 == 0 {
+			n = n/2
+		} else {
+			n = n*3 + 1
+		}
+	}
+	return i;
 }
