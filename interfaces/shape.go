2b69ac8624e6cae3c525fdbb7ee293d0f0cd6339
Santhosh Kumar M <saam.cit@gmail.com>
2016-07-23 10:57:46 +0530

    Fixing AreaofShapes

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,29 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct {
+	side int
+}
+
+type Rectangle struct {
+	length int
+	breadth int
+}
+
+type Hybrid struct {
+	shape1 Shape
+	shape2 Shape
+}
+
+func (square Square) Area() int {
+	return square.side * square.side
+}
+
+func (rectangle Rectangle) Area() int {
+	return rectangle.length*rectangle.breadth
+}
+
+func (hybrid Hybrid) Area() int {
+	return hybrid.shape1.Area() + hybrid.shape2.Area()
+}
